e299d2341ecda284b1e93254c56f1ac6dac407e0
nastala <sezginyagli@gmail.com>
2018-06-13 03:31:26 +0300

    Bonus eklendi

--- app/src/main/java/com/example/nastala/ovidoschallenge/AlbumDetailsActivity.java
+++ app/src/main/java/com/example/nastala/ovidoschallenge/AlbumDetailsActivity.java
@@ -1,6 +1,8 @@
 package com.example.nastala.ovidoschallenge;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -36,6 +38,8 @@ public class AlbumDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_album_details);
+
+        Log.d(TAG, "Orientation: " + String.valueOf(AlbumDetailsActivity.this.getResources().getConfiguration().orientation));
 
         Intent intent = getIntent();
         if(intent.getExtras() != null)
@@ -92,6 +96,11 @@ public class AlbumDetailsActivity extends AppCompatActivity {
     }
 
     private void fillGvAlbumDetails() {
+        if(AlbumDetailsActivity.this.getResources().getConfiguration().orientation == 2)
+            gvAlbumDetails.setNumColumns(3);
+        else
+            gvAlbumDetails.setNumColumns(2);
+
         AlbumDetailsGVAdapter albumDetailsGVAdapter = new AlbumDetailsGVAdapter(this, photos);
         gvAlbumDetails.setAdapter(albumDetailsGVAdapter);
         pbAlbumDetails.setVisibility(View.GONE);
